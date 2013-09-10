b302913bee94bef4b7d0dcf16c8c1a5bbdce0036
Ruben Daniels <ruben@c9.io>
2013-09-10 20:23:26 +0000

    Renamed more events

--- editor.js
+++ editor.js
@@ -43,7 +43,7 @@ define(function(require, exports, module) {
             plugin.on("load", function(){
             });
             
-            plugin.on("document.load", function(e){
+            plugin.on("documentLoad", function(e){
                 var session = e.doc.getSession();
                 
                 if (session.repl) return;
@@ -54,12 +54,12 @@ define(function(require, exports, module) {
                     message   : e.state.message
                 });
             });
-            plugin.on("document.activate", function(e){
+            plugin.on("documentActivate", function(e){
                 currentSession = e.doc.getSession();
                 if (ace) 
                     currentSession.repl.attach(ace);
             });
-            plugin.on("document.unload", function(e){
+            plugin.on("documentUnload", function(e){
                 var session = e.doc.getSession();
                 session.repl.detach();
                 delete session.repl;
