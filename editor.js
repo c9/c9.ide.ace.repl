ada99852fae4831d066b19ea128bf23de12fdd58
nightwing <amirjanyan@gmail.com>
2015-02-15 02:00:58 +0400

    remove incorrect license information from comments

--- editor.js
+++ editor.js
@@ -1,9 +1,3 @@
-/**
- * Text Editor for the Cloud9
- *
- * @copyright 2010, Ajax.org B.V.
- * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
- */
 define(function(require, exports, module) {
     main.consumes = ["editors"];
     main.provides = ["ace.repl"];
